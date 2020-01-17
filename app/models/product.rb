f065ba0d7ba3fdf5403c8e0925f77000212787c4
Angela Weber <angelaweber95@hotmail.com>
2020-01-17 13:52:48 -0800

    add and pass validation test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, :cost, :country_of_origin, presence: true
-  # validates :cost, presence: true
-  # validates :country_of_origin, presence: true
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true
 end
