cb45d9d69230a1e78e408942cb52c26c363fb6c2
Angela Weber <angelaweber95@hotmail.com>
2020-01-17 16:06:42 -0800

    attempt to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#
+Product.destroy_all
+Review.destroy_all
+# 50.times do |index|
+#   Product.create!(name: Faker::Games::Witcher.monster,
+#     cost: Faker::Number.decimal(r_digits: 2),
+#     country_of_origin: Faker::WorldCup.team)
+#     # binding.pry
+    300.times do |index|
+      50.times do |index|
+        Product.create!(name: Faker::Games::Witcher.monster,
+          cost: Faker::Number.decimal(r_digits: 2),
+          country_of_origin: Faker::WorldCup.team)
+      Review.create!(author: Faker::TvShows::BojackHorseman.character,
+        content_body: Faker::Hipster.paragraph(sentence_count: 3), rating: Faker::Number.within(range: 1..5),
+        product_id: Product.ids)
+      end
+    end
+
+
+    p "Created #{Review.count} reviews, and #{Product.count}"
