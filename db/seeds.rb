6f554bc063466b06b9daca2495e9f3c3dd26863d
Angela Weber <angelaweber95@hotmail.com>
2020-01-20 11:07:23 -0800

    add review and product count to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 
 Product.destroy_all
 
@@ -23,4 +16,4 @@ Product.destroy_all
 
 
 
-    p "Created #{Review.count} reviews, and #{Product.count}"
+    p "Created #{Product.count} products, and #{Review.count} reviews"
